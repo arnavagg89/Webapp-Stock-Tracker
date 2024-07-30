1519e984599fba7a3f7295381d0a4a3c07a9cba5
Arnav Aggarwal <arnavagg89@gmail.com>
2024-07-30 16:48:09 +0530

    init transactions model and view

--- webapp_stock_tracker/stock_transactions/models.py
+++ webapp_stock_tracker/stock_transactions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    stock_symbol = models.CharField(max_length=10)
+    price_per_share = models.DecimalField(max_digits=10, decimal_places=2)
+    number_of_shares = models.IntegerField()
+    transaction_date = models.DateField()
+    broker_fee = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.stock_symbol} - {self.transaction_date}"
